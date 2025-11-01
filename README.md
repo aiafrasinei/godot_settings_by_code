99944494ab0846548ed84100597e52333bfbb927
aiaf <alexandruafrasinei@gmail.com>
2025-11-01 20:18:43 +0200

    Added settings project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # godot_settings_by_code
+
+Project for godot4.
+
+Example for a game settings app that save the data to cfg file (gdscript).
---
+++ settings.gd
@@ -0,0 +1,16 @@
+extends Node
+
+# Create new ConfigFile object.
+var config = ConfigFile.new()
+
+func _on_tree_entered() -> void:
+	print(OS.get_user_data_dir())
+	config.set_value("Settings", "Fullscreen", false)
+	config.set_value("Settings", "Resolution", "Desktop")
+	config.save("user://settings.cfg")
+
+
+func _on_tree_exited() -> void:
+	config.set_value("Settings", "Fullscreen", $Panel/TabContainer/Settings/MarginContainer/VFlowContainer/Fullscreen.toggle_mode)
+	config.set_value("Settings", "Resolution", $Panel/TabContainer/Settings/MarginContainer/VFlowContainer/OptionButton.text)
+	config.save("user://settings.cfg")
